1549c041616e5c88e72f7784541a4c9a4b1a5f47
Ryan Wheale <ryan.wheale@gmail.com>
2016-01-01 18:48:25 -0700

    Fix serialization issues around special html entities

--- test/serializer-test.js
+++ test/serializer-test.js
@@ -9,23 +9,30 @@ QUnit.module('Serializer', {
   }
 });
 
-QUnit.test('serializes correctly', function (assert) {
+QUnit.test('serializes text nodes correctly', function (assert) {
   var actual = this.serializer.serialize(fragment(
     element('div', { id:'foo' },
       element('b', {},
-        text('Foo & Bar')
+        text('Foo & Bar &amp; Baz < Buz > Biz')
       )
     )
   ));
-  assert.equal(actual, '<div id="foo"><b>Foo &amp; Bar</b></div>');
+  assert.equal(actual, '<div id="foo"><b>Foo &amp; Bar &amp; Baz &lt; Buz &gt; Biz</b></div>');
+});
+
+QUnit.test('serializes attribute values correctly', function (assert) {
+  var actual = this.serializer.serialize(fragment(
+    element('div', { title:'"foo & bar &amp; baz < buz > biz"' })
+  ));
+  assert.equal(actual, '<div title="&quot;foo &amp; bar &amp;amp; baz < buz > biz&quot;"></div>');
 });
 
 QUnit.test('serializes textContent', function(assert) {
   var el, actual, frag;
 
   el = element('div', {});
-  el.textContent = 'hello world';
+  el.textContent = 'hello > world &amp; goodbye';
   actual = this.serializer.serialize(fragment(el));
 
-  assert.equal(actual, '<div>hello world</div>');
+  assert.equal(actual, '<div>hello &gt; world &amp;amp; goodbye</div>');
 });
