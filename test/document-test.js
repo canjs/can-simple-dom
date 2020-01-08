93590073fb0e03e83c9cab9aa2b231495ca727fd
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2020-01-08 17:31:56 +0100

    Node should report it contains itself

--- test/document-test.js
+++ test/document-test.js
@@ -12,3 +12,10 @@ unit.test('Document should contain appended Elements', function (assert) {
 	document.body.removeChild(element);
 	assert.notOk(document.contains(element), 'document should not contain element');
 });
+
+unit.test('Allow a Node to report that it contains itself', function(assert) {
+	var element = document.createElement('div');
+	document.body.appendChild(element);
+	assert.ok(element.contains(element), 'An element contains itself');
+	document.body.removeChild(element);
+});
